4910a410c56e9513e6560ef0bcd1a9aa2c12a870
5F\ktc <>
2025-11-14 19:13:54 +0900

    攻撃Ⅰ仮完成実装

--- mekagame/Assets/script/enemyattack.cs
+++ mekagame/Assets/script/enemyattack.cs
@@ -5,23 +5,22 @@ public class enemyattack : MonoBehaviour
 {
     [SerializeField] GameObject missile;//ミサイル攻撃のオブジェクト
     [SerializeField] GameObject attackpoint;//攻撃発生地点
+    [SerializeField] GameObject lazer;
+    [SerializeField] GameObject lazerattackpoint;
 
     [SerializeField] float rndm;//フィールドごとの範囲指定マイナス
     [SerializeField] float rndp;//フィールドごとの範囲指定プラス
 
     [SerializeField] int attackf;//攻撃の間隔
 
-    [SerializeField] int attackpointx;//攻撃発生左右
     [SerializeField] int attackpointy;//攻撃発生の高さ
     [SerializeField] int attackpointz;//攻撃発生の奥行
 
-
-
+    [SerializeField] int lazerpointy;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
         float x = Random.Range(rndm, rndp);//地面の広さによって変更
         float z = Random.Range(rndm, rndp);//地面の広さによって変更
         for (int i = 0; i < 6; i++)
@@ -36,18 +35,24 @@ public class enemyattack : MonoBehaviour
 
             Instantiate(missile, new Vector3((attackf * numx) - numx, attackpointy, (attackf * numz) - numz), Quaternion.identity);//発射
             Instantiate(attackpoint, new Vector3((attackf * numx) - numx, 0, (attackf * numz) - numz), Quaternion.identity);//攻撃範囲
-
         }
+        Instantiate(lazerattackpoint, new Vector3(x, 0, 0), Quaternion.identity);
+        Instantiate(lazer, new Vector3(x, lazerpointy, 0), Quaternion.identity);//発射
 
-
-        
+        /*攻撃3kari
+        for (int i = 0; i < 範囲次第; i++)
+        {
+            Instantiate(missile, new Vector3(attackpointx - i * attackf, attackpointy, attackpointz - i * attackf), Quaternion.identity);
+            Instantiate(attackpoint, new Vector3(attackpointx + i * attackf, 0, attackpointz + i * attackf), Quaternion.identity);
+            Instantiate(missile, new Vector3(attackpointx - i * attackf, attackpointy, attackpointz - i * attackf), Quaternion.identity);
+            Instantiate(attackpoint, new Vector3(attackpointx + i * attackf, 0, attackpointz + i * attackf), Quaternion.identity);
+        }
+        */
     }
 
-    
     // Update is called once per frame
     void Update()
     {
         Rigidbody ballRigidbody = missile.GetComponent<Rigidbody>();
-
     }
 }
