494d436030c715a448ab586b74aa92057101f1c6
5F\ktc <>
2025-11-21 15:53:15 +0900

    攻撃Ⅲ実装

--- mekagame/Assets/script/enemyattack.cs
+++ mekagame/Assets/script/enemyattack.cs
@@ -10,9 +10,7 @@ public class enemyattack : MonoBehaviour
 
     [SerializeField] float rndm;//フィールドごとの範囲指定マイナス
     [SerializeField] float rndp;//フィールドごとの範囲指定プラス
-    public float x;
-    public float z;
-
+    
     [SerializeField] int attackf;//攻撃の間隔
 
     [SerializeField] int attack1missile;
@@ -26,11 +24,12 @@ public class enemyattack : MonoBehaviour
     [SerializeField] int attackpointy;//攻撃発生の高さ
     [SerializeField] int attackpointz;//攻撃発生の奥行
 
-    
-
     [SerializeField] int lazerpointy;
 
-    
+    public float x;
+    public float z;
+
+    public int attack3bunki;//random値確認用基本使わない
 
     void Attack1()
     {
@@ -64,27 +63,33 @@ public class enemyattack : MonoBehaviour
     }
     void Attack3()
     {
+        
         //攻撃3kari
-        for (int i = 1; i <= attack3missilex; i++)
+        if (attack3bunki < 50)
         {
-            Instantiate(missile, new Vector3(attackpointx - i * attackf, attackpointy, attackpointz - i * attackf), Quaternion.identity);
-            Instantiate(attackpoint, new Vector3(attackpointx - i * attackf, 0, attackpointz - i * attackf), Quaternion.identity);
-            Instantiate(missile, new Vector3(attackpointx - i * attackf, attackpointy, attackpointz + i * attackf), Quaternion.identity);
-            Instantiate(attackpoint, new Vector3(attackpointx - i * attackf, 0, attackpointz + i * attackf), Quaternion.identity);
-            Instantiate(missile, new Vector3(attackpointx + i * attackf, attackpointy, attackpointz - i * attackf), Quaternion.identity);
-            Instantiate(attackpoint, new Vector3(attackpointx + i * attackf, 0, attackpointz - i * attackf), Quaternion.identity);
-            Instantiate(missile, new Vector3(attackpointx + i * attackf, attackpointy, attackpointz + i * attackf), Quaternion.identity);
-            Instantiate(attackpoint, new Vector3(attackpointx + i * attackf, 0, attackpointz + i * attackf), Quaternion.identity);
+            for (int i = 1; i <= attack3missilex; i++)
+            {
+                Instantiate(missile, new Vector3(attackpointx - i * attackf, attackpointy, attackpointz - i * attackf), Quaternion.identity);
+                Instantiate(attackpoint, new Vector3(attackpointx - i * attackf, 0, attackpointz - i * attackf), Quaternion.identity);
+                Instantiate(missile, new Vector3(attackpointx - i * attackf, attackpointy, attackpointz + i * attackf), Quaternion.identity);
+                Instantiate(attackpoint, new Vector3(attackpointx - i * attackf, 0, attackpointz + i * attackf), Quaternion.identity);
+                Instantiate(missile, new Vector3(attackpointx + i * attackf, attackpointy, attackpointz - i * attackf), Quaternion.identity);
+                Instantiate(attackpoint, new Vector3(attackpointx + i * attackf, 0, attackpointz - i * attackf), Quaternion.identity);
+                Instantiate(missile, new Vector3(attackpointx + i * attackf, attackpointy, attackpointz + i * attackf), Quaternion.identity);
+                Instantiate(attackpoint, new Vector3(attackpointx + i * attackf, 0, attackpointz + i * attackf), Quaternion.identity);
+            }
         }
-
-        for (int i = 1; i <= attack3missilety; i++)
+        else
         {
-            Instantiate(missile, new Vector3(0, attackpointy, attackpointz3ty - i * attackf), Quaternion.identity);
-            Instantiate(attackpoint, new Vector3(0, 0, attackpointz3ty - i * attackf), Quaternion.identity);
-            Instantiate(missile, new Vector3(attackpointx3ty - i * attackf, attackpointy, 0), Quaternion.identity);
-            Instantiate(attackpoint, new Vector3(attackpointx3ty - i * attackf, 0, 0), Quaternion.identity);
+            for (int i = 1; i <= attack3missilety; i++)
+            {
+                Instantiate(missile, new Vector3(0, attackpointy, attackpointz3ty - i * attackf), Quaternion.identity);
+                Instantiate(attackpoint, new Vector3(0, 0, attackpointz3ty - i * attackf), Quaternion.identity);
+                Instantiate(missile, new Vector3(attackpointx3ty - i * attackf, attackpointy, 0), Quaternion.identity);
+                Instantiate(attackpoint, new Vector3(attackpointx3ty - i * attackf, 0, 0), Quaternion.identity);
+            }
         }
-
+            
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -92,6 +97,7 @@ public class enemyattack : MonoBehaviour
     {
         x = Random.Range(rndm, rndp);//地面の広さによって変更
         z = Random.Range(rndm, rndp);//地面の広さによって変更
+        attack3bunki = Random.Range(0, 100);
 
         Invoke("Attack1", 3f);
       //Invoke("Attack2lp", 7f);
