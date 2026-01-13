d3e3b0371be3aa223b9da266499bcfe4cf00330c
MinakuchiRyosuke <ktc23a31a0043@edu.kyoto-tech.ac.jp>
2026-01-13 10:03:48 +0900

    敵のモデル

---
+++ mekagame/Assets/Prefab/DKP/CombatDrone002/Demo/Scripts/CD_DemoCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CD_DemoCamera : MonoBehaviour {
+
+	public KeyCode SpinCameraLeftKey = KeyCode.A;
+	public KeyCode SpinCameraRightKey = KeyCode.D;
+
+	public float SpinSpeed = 2.0f;
+
+	private Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (SpinCameraLeftKey)) {
+			myTransform.RotateAround (myTransform.position, myTransform.up, SpinSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (SpinCameraRightKey)) {
+			myTransform.RotateAround (myTransform.position, myTransform.up, -(SpinSpeed * Time.deltaTime));
+		}
+
+	}
+}
---
+++ mekagame/Assets/Prefab/DKP/CombatDrone002/Demo/Scripts/CD_LookAtTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CD_LookAtTarget : MonoBehaviour {
+
+	public Transform LookAtTargetTransform;
+
+	private Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (LookAtTargetTransform != null) {
+			myTransform.LookAt (LookAtTargetTransform.position, Vector3.up);
+		}
+	}
+}
