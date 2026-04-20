a7bc09dabc634ed407df52fb9488ac51b51448fa
GTUNE-\ktc <>
2026-04-20 19:50:58 +0900

    アイテム

---
+++ mekagame/Assets/Item/Scripts/ItemFall.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ItemFall : MonoBehaviour
+{
+    //プレハブ
+    [Header("アイテム")]
+    [SerializeField] GameObject a;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
