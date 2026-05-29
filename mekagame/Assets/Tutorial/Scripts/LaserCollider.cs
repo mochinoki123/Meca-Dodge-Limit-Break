14ab16e87df02bade39f0d5cb18b4f36a74b5e97
MinakuchiRyosuke <ktc23a31a0043@edu.kyoto-tech.ac.jp>
2026-05-29 19:40:20 +0900

    no message

--- mekagame/Assets/Tutorial/Scripts/LaserCollider.cs
+++ mekagame/Assets/Tutorial/Scripts/LaserCollider.cs
@@ -5,26 +5,17 @@ using System.Collections;
 public class LaserCollider : MonoBehaviour
 {
     [SerializeField] private Transform laser;
-    [SerializeField] private float maxPos = 100f;
 
     private void OnEnable()
     {
-        StartCoroutine(GrowCollider());
     }
-    public IEnumerator GrowCollider()
+    private void Update()
     {
-        while (transform.position.x < maxPos)
-        {
-            float grow = laser.transform.localScale.x * 2f;
-            transform.position = new Vector3(grow, 2f, 0f);
-
-            yield return null;
-        }
-        yield return null;
-        transform.localScale = new Vector3(maxPos, transform.localScale.y, transform.localScale.z);
+        float grow = laser.localScale.x * 2f;
+        transform.localPosition = new Vector3(-grow, 0f, 0f);
     }
     private void OnDisable()
     {
-        transform.position = new Vector3(0f, 2f, 0f);
+        transform.localPosition = new Vector3(0f, 2f, 0f);
     }
 }
