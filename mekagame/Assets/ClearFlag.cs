cee9010a4819b3f028151704fdd18f74462437cc
RyosukeMinakuchi <ktc23a31a0043@edu.kyoto-tech.ac.jp>
2026-07-31 11:06:26 +0900

    ClearFlag.cs を更新

--- mekagame/Assets/ClearFlag.cs
+++ mekagame/Assets/ClearFlag.cs
@@ -1,10 +1,12 @@
+//クリアフラグのスクリプダブルオブジェクト
+//R3で変数を監視しているコードに値が変わったら通知がいく
 using UnityEngine;
 using R3;
 
 [CreateAssetMenu(fileName = "ClearFlag", menuName = "Scriptable Objects/ClearFlag")]
 public class ClearFlag : ScriptableObject
 {
-    [SerializeField] private string flagName;
+    [SerializeField] private string flagName;  //プラグの名前
 
     [SerializeField] private SerializableReactiveProperty<bool> isGameCleared = new(false);
     [SerializeField] private SerializableReactiveProperty<bool> isPhaseCleared = new(false);
