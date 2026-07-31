b74d349fca168dabc4a4fb1d9e922330345b1e07
RyosukeMinakuchi <ktc23a31a0043@edu.kyoto-tech.ac.jp>
2026-07-31 10:59:46 +0900

    ITutorialTask.cs を更新

--- mekagame/Assets/Tutorial/Scripts/ITutorialTask.cs
+++ mekagame/Assets/Tutorial/Scripts/ITutorialTask.cs
@@ -1,11 +1,12 @@
+／／チュートリアル用インターフェイス
 public interface ITutorialTask
 {
-    string Title { get; }
-    string Description { get; }
-    float TransitionTime { get; }
-    void OnTaskSet();
-    void OnTaskEnd();
-    void Tick();
-    bool IsCompleted();
-    string GetProgress();
+    string Title { get; }  //タスク名
+    string Description { get; }  //タスク内容
+    float TransitionTime { get; }  //次のタスクに移るまでの時間
+    void OnTaskSet();  //タスク開始
+    void OnTaskEnd();  //タスク終了
+    void Tick();  //条件判定、進行状況の更新など
+    bool IsCompleted();  //タスクが完了したかどうか
+    string GetProgress();  //現在の進捗
 }
