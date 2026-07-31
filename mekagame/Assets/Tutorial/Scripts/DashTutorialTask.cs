eb66e01d503ca71b3cd4a9c4cd4becb7d6715b12
RyosukeMinakuchi <ktc23a31a0043@edu.kyoto-tech.ac.jp>
2026-07-31 11:55:55 +0900

    DashTutorialTask.cs を更新

--- mekagame/Assets/Tutorial/Scripts/DashTutorialTask.cs
+++ mekagame/Assets/Tutorial/Scripts/DashTutorialTask.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine.InputSystem;
+//ダッシュのチュートリアル用
+using UnityEngine.InputSystem;
 
 public class DashTutorialTask : InputTutorialTask
 {
@@ -10,23 +11,25 @@ public class DashTutorialTask : InputTutorialTask
 
     public DashTutorialTask(PlayerInput playerInput) : base(playerInput) { }
 
+　　　//初期化処理
     public override void OnTaskSet()
     {
         base.OnTaskSet();
         dashed = false;
-        dashAction = GetAction("Sprint");
+        dashAction = GetAction("Sprint");//ダッシュ用のインプットアクション取得
         if (dashAction != null)
-            dashAction.performed += OnDash;
+            dashAction.performed += OnDash;//ダッシュ入力時にOnDashを呼ぶイベント登録
     }
 
     public override void Tick() { }
 
+    //CallbackContextは入力の詳細情報構造体
     private void OnDash(InputAction.CallbackContext ctx) => dashed = true;
 
     public override void OnTaskEnd()
     {
         if (dashAction != null)
-            dashAction.performed -= OnDash;
+            dashAction.performed -= OnDash;//イベント解除
     }
 
     public override bool IsCompleted() => dashed;
