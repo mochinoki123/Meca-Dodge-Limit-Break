e94f668b8ace6356cd3cfe216efc583f9fdbbe14
HarutoM83 <ktc24a32a0010@edu.kyoto-tech.ac.jp>
2026-08-08 12:39:37 +0900

    レーザーAoE改良

--- mekagame/Assets/Tutorial/Scripts/AoE.cs
+++ mekagame/Assets/Tutorial/Scripts/AoE.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using System.Collections;
 
 public class AoE : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float speed;
+    [SerializeField] private float maxScale;
+
+    private void OnEnable()
     {
-        
+        StartCoroutine(Grow());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Grow()
     {
-        
+        yield return new WaitForSeconds(0.1f);
+        while (transform.localScale.z < maxScale)
+        {
+            float newX = transform.localScale.x + (speed * Time.deltaTime);
+
+            transform.localScale = new Vector3(newX, transform.localScale.y, transform.localScale.z);
+
+            yield return null;
+        }
+
+        transform.localScale = new Vector3(maxScale, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void OnDisable()
+    {
+        transform.localScale = new Vector3(0.01f, 4f, 4f);
     }
 }
